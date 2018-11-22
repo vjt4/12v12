103b72774071523fcb7fd9820c8f4c6f7b36b476
Niker323 <39685652+Niker323@users.noreply.github.com>
2018-11-22 11:16:44 +0300

    Create

---
+++ game/dota_addons/new10v10/scripts/vscripts/addon_game_mode.lua
@@ -0,0 +1,78 @@
+-- Rebalance the distribution of gold and XP to make for a better 10v10 game
+local GOLD_SCALE_FACTOR_INITIAL = 1
+local GOLD_SCALE_FACTOR_FINAL = 2.5
+local GOLD_SCALE_FACTOR_FADEIN_SECONDS = (60 * 60) -- 60 minutes
+local XP_SCALE_FACTOR_INITIAL = 2
+local XP_SCALE_FACTOR_FINAL = 2
+local XP_SCALE_FACTOR_FADEIN_SECONDS = (60 * 60) -- 60 minutes
+
+if CMegaDotaGameMode == nil then
+	_G.CMegaDotaGameMode = class({}) -- put CMegaDotaGameMode in the global scope
+	--refer to: http://stackoverflow.com/questions/6586145/lua-require-with-global-local
+end
+
+function Activate()
+	CMegaDotaGameMode:InitGameMode()
+end
+
+function CMegaDotaGameMode:InitGameMode()
+	print( "10v10 Mode Loaded!" )
+
+	-- Adjust team limits
+	GameRules:SetCustomGameTeamMaxPlayers( DOTA_TEAM_GOODGUYS, 10 )
+	GameRules:SetCustomGameTeamMaxPlayers( DOTA_TEAM_BADGUYS, 10 )
+	GameRules:SetStrategyTime( 0.0 )
+	GameRules:SetShowcaseTime( 0.0 )
+
+	-- Hook up gold & xp filters
+	GameRules:GetGameModeEntity():SetModifyGoldFilter( Dynamic_Wrap( CMegaDotaGameMode, "FilterModifyGold" ), self )
+	GameRules:GetGameModeEntity():SetModifyExperienceFilter( Dynamic_Wrap(CMegaDotaGameMode, "FilterModifyExperience" ), self )
+	GameRules:GetGameModeEntity():SetBountyRunePickupFilter( Dynamic_Wrap(CMegaDotaGameMode, "FilterBountyRunePickup" ), self )
+	GameRules:GetGameModeEntity():SetTowerBackdoorProtectionEnabled( true )
+	GameRules:SetGoldTickTime( 0.3 ) -- default is 0.6
+
+	self.m_CurrentGoldScaleFactor = GOLD_SCALE_FACTOR_INITIAL
+	self.m_CurrentXpScaleFactor = XP_SCALE_FACTOR_INITIAL
+	GameRules:GetGameModeEntity():SetThink( "OnThink", self, 5 ) 
+end
+
+function CMegaDotaGameMode:OnThink()
+	if GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
+		-- update the scale factor: 
+	 	-- * SCALE_FACTOR_INITIAL at the start of the game
+		-- * SCALE_FACTOR_FINAL after SCALE_FACTOR_FADEIN_SECONDS have elapsed
+		local curTime = GameRules:GetDOTATime( false, false )
+		local goldFracTime = math.min( math.max( curTime / GOLD_SCALE_FACTOR_FADEIN_SECONDS, 0 ), 1 )
+		local xpFracTime = math.min( math.max( curTime / XP_SCALE_FACTOR_FADEIN_SECONDS, 0 ), 1 )
+		self.m_CurrentGoldScaleFactor = GOLD_SCALE_FACTOR_INITIAL + (goldFracTime * ( GOLD_SCALE_FACTOR_FINAL - GOLD_SCALE_FACTOR_INITIAL ) )
+		self.m_CurrentXpScaleFactor = XP_SCALE_FACTOR_INITIAL + (xpFracTime * ( XP_SCALE_FACTOR_FINAL - XP_SCALE_FACTOR_INITIAL ) )
+--		print( "Gold scale = " .. self.m_CurrentGoldScaleFactor )
+--		print( "XP scale = " .. self.m_CurrentXpScaleFactor )
+	end
+	return 5
+end
+
+
+function CMegaDotaGameMode:FilterBountyRunePickup( filterTable )
+--	print( "FilterBountyRunePickup" )
+--  for k, v in pairs( filterTable ) do
+--  	print("MG: " .. k .. " " .. tostring(v) )
+--  end
+	filterTable["gold_bounty"] = self.m_CurrentGoldScaleFactor * filterTable["gold_bounty"]
+	filterTable["xp_bounty"] = self.m_CurrentXpScaleFactor * filterTable["xp_bounty"]
+	return true
+end
+
+function CMegaDotaGameMode:FilterModifyGold( filterTable )
+--	print( "FilterModifyGold" )
+--	print( self.m_CurrentGoldScaleFactor )
+	filterTable["gold"] = self.m_CurrentGoldScaleFactor * filterTable["gold"]
+	return true
+end
+
+function CMegaDotaGameMode:FilterModifyExperience( filterTable )
+--	print( "FilterModifyExperience" )
+--	print( self.m_CurrentXpScaleFactor )
+	filterTable["experience"] = self.m_CurrentXpScaleFactor * filterTable["experience"]
+	return true
+end
