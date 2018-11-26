130fcf0ad9f7458cf145054660c89d60b398b29f
Niker323 <39685652+Niker323@users.noreply.github.com>
2018-11-26 22:27:57 +0300

    Disable Help Button

--- game/dota_addons/new10v10/scripts/vscripts/addon_game_mode.lua
+++ game/dota_addons/new10v10/scripts/vscripts/addon_game_mode.lua
@@ -36,6 +36,18 @@ function CMegaDotaGameMode:InitGameMode()
 	GameRules:GetGameModeEntity():SetThink( "OnThink", self, 5 ) 
 end
 
+CustomGameEventManager:RegisterListener("set_disable_help", function(_, data)
+	local to = data.to;
+	if PlayerResource:IsValidPlayerID(to) then
+		local playerId = data.PlayerID;
+		local disable = data.disable == 1
+		PlayerResource:SetUnitShareMaskForPlayer(playerId, to, 4, disable)
+ 		local disableHelp = CustomNetTables:GetTableValue("disable_help", tostring(playerId)) or {}
+		disableHelp[tostring(to)] = disable
+		CustomNetTables:SetTableValue("disable_help", tostring(playerId), disableHelp)
+	end
+end)
+
 function CMegaDotaGameMode:OnThink()
 	if GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
 		-- update the scale factor: 
