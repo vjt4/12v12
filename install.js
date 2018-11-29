e9410e8b4b57934e1e9edde840f907fae0c91a78
ark120202 <ark120202@gmail.com>
2018-11-29 18:25:42 +0500

    refactor(*): update project structure to match overthrow's

---
+++ install.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface(process.stdin, process.stdout);
+rl.setPrompt('Dota Path: ');
+rl.prompt();
+
+rl.on('line', (dotaPath) => {
+  const gamePath = `${dotaPath}/game/dota_addons`;
+  const contentPath = `${dotaPath}/content/dota_addons`;
+  if (!fs.existsSync(gamePath)) {
+    console.log(`${gamePath} not found`);
+    rl.prompt();
+    return;
+  }
+  if (!fs.existsSync(contentPath)) {
+    console.log(`${contentPath} not found`);
+    rl.prompt();
+    return;
+  }
+  rl.close();
+
+  (async () => {
+    await fs.promises.rename('./game', gamePath + '/12v12');
+    await fs.promises.rename('./content', contentPath + '/12v12');
+    await fs.promises.symlink(gamePath + '/12v12', './game', 'junction');
+    await fs.promises.symlink(contentPath + '/12v12', './content', 'junction');
+  })().catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
+});
