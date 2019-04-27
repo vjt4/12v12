a2de2ed57c279d13163a82d86b050f6ac1bbcc69
Niker323 <artiom123.budko@yandex.by>
2019-04-27 16:23:28 +0300

    Tether not work with disable help

--- game/scripts/vscripts/addon_game_mode.lua
+++ game/scripts/vscripts/addon_game_mode.lua
@@ -536,7 +536,7 @@ function CMegaDotaGameMode:ExecuteOrderFilter(filterTable)
 
 	if order_type == DOTA_UNIT_ORDER_CAST_TARGET then
 		if ability and target and unit then
-			if PlayerResource:IsDisableHelpSetForPlayerID(target:GetPlayerOwnerID(), unit:GetPlayerOwnerID()) and (ability:GetName() == "oracle_fates_edict" or ability:GetName() == "oracle_purifying_flames") then
+			if PlayerResource:IsDisableHelpSetForPlayerID(target:GetPlayerOwnerID(), unit:GetPlayerOwnerID()) and (ability:GetName() == "oracle_fates_edict" or ability:GetName() == "oracle_purifying_flames" or ability:GetName() == "wisp_tether") then
 				DisplayError(unit:GetPlayerOwnerID(), "dota_hud_error_target_has_disable_help")
 				return false
 			end
