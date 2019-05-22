28be00fafbfe7220fbc0c936d6429c44613ae722
Shesmuu <50734587+Shesmuu@users.noreply.github.com>
2019-05-22 18:02:59 +0500

    Patreon only

--- game/scripts/vscripts/high_five_and_banner.lua
+++ game/scripts/vscripts/high_five_and_banner.lua
@@ -13,7 +13,16 @@ end, nil )
 
 CustomGameEventManager:RegisterListener( "cons_ability_activate", function( id, keys )
 	local npc = EntIndexToHScript( keys.unit )
+
+	local p = Patreons:GetPlayerSettings( npc:GetPlayerID() )
+
 	if not npc then return end
+
+	if p and p.level == 0 and keys.ability == "seasonal_ti9_banner" then
+		CustomGameEventManager:Send_ServerToPlayer( PlayerResource:GetPlayer( npc:GetPlayerID() ), "display_custom_error", { message = "#nopatreonerror" } )
+		return
+	end
+
 	local ability = npc:FindAbilityByName( keys.ability )
 
 	if ability then
