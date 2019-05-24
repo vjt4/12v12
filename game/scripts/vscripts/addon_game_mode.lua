4b1effb7bf3b96f09e71a47c5025c3160990bde3
Curtis Dubois <curtisjdubois@hotmail.com>
2019-05-24 13:12:26 +1000

    mini fury swipes for fountain

--- game/scripts/vscripts/addon_game_mode.lua
+++ game/scripts/vscripts/addon_game_mode.lua
@@ -397,6 +397,7 @@ function CMegaDotaGameMode:OnGameRulesStateChange(keys)
         
         local toAdd = {
             luna_moon_glaive_fountain = 4,
+            ursa_fury_swipes_fountain = 1,
         }
 
         local fountains = Entities:FindAllByClassname('ent_dota_fountain')
