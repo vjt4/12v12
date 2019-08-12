2f100cbc8e7ab03a78f64bd1e0ea3ab8f7342ae6
Shesmuu <50734587+Shesmuu@users.noreply.github.com>
2019-08-13 01:30:18 +0500

    Update creator.js

--- content/panorama/scripts/custom_game/cosmetic_abilities/creator.js
+++ content/panorama/scripts/custom_game/cosmetic_abilities/creator.js
@@ -19,21 +19,24 @@ var hud = $.GetContextPanel().GetParent().GetParent().GetParent()
 var lower_hud = hud.FindChildTraverse( "HUDElements" ).FindChild( "lower_hud" )
 var center_with_stats = lower_hud.FindChild( "center_with_stats" )
 var center_block = center_with_stats.FindChild( "center_block" )
+var buff_container = lower_hud.FindChild( "BuffContainer" )
 
 lower_hud.style.height = "100%"
 center_with_stats.style.height = "100%"
 center_block.style.height = "100%"
 
-lower_hud.FindChild( "buffs" ).style.transform = "translateY( -50px )"
-lower_hud.FindChild( "debuffs" ).style.transform = "translateY( -50px )"
+buff_container.FindChild( "buffs" ).style.transform = "translateY( -50px )"
+buff_container.FindChild( "debuffs" ).style.transform = "translateY( -50px )"
 
-var newPanel = $.CreatePanel( "Panel", center_block, "CosmeticAbilities" )
-newPanel.BLoadLayout( "file://{resources}/layout/custom_game/cosmetic_abilities.xml", false, false )
-center_block.MoveChildBefore( newPanel, center_block.FindChild( "center_bg" ) )
+if ( !center_block.FindChild( "CosmeticAbilities" ) ) {
+	var newPanel = $.CreatePanel( "Panel", center_block, "CosmeticAbilities" )
+	newPanel.BLoadLayout( "file://{resources}/layout/custom_game/cosmetic_abilities.xml", false, false )
+	center_block.MoveChildBefore( newPanel, center_block.FindChild( "center_bg" ) )
+}
 
 center_block.FindChildrenWithClassTraverse( "TertiaryAbilityContainer" )[0].style.visibility = "collapse"
 
-var abilities_panel = center_block.FindChild( "AbilitiesAndStatBranch" ).FindChild( "abilities" )
+var abilities_panel = center_block.FindChild( "AbilitiesAndStatBranch" ).FindChildTraverse( "abilities" )
 
 function HideAbilities() {
 	var abilities = abilities_panel.Children()
