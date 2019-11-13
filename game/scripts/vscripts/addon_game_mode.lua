41c4e286fbb79d1b2fcff016633ae3f65b8b79fd
Curtis Dubois <curtisjdubois@hotmail.com>
2019-11-14 02:40:36 +1100

    Fix showing right disconnected player

--- game/scripts/vscripts/addon_game_mode.lua
+++ game/scripts/vscripts/addon_game_mode.lua
@@ -967,7 +967,7 @@ function CMegaDotaGameMode:OnConnectFull(data)
 end
 
 function CMegaDotaGameMode:OnPlayerDisconnect(data)
-	CustomGameEventManager:Send_ServerToAllClients( "change_leave_status", {leave = true, playerId = data.userid} )
+	CustomGameEventManager:Send_ServerToAllClients( "change_leave_status", {leave = true, playerId = data.userid-1} )
 end
 
 function CMegaDotaGameMode:ExecuteOrderFilter(filterTable)
