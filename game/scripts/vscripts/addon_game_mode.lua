0a79b10629f0c56cf034ed8446caa8118b180b3a
Sheodar <kkacbachok@gmail.com>
2020-01-09 09:14:19 +0200

    Remove custom GPM system (now players give gold through courier)

--- game/scripts/vscripts/addon_game_mode.lua
+++ game/scripts/vscripts/addon_game_mode.lua
@@ -25,7 +25,6 @@ local NET_WORSE_FOR_RAPIER_MIN = 20000
 
 require("common/init")
 require("util")
-require("gpm_lib")
 
 WebApi.customGame = "Dota12v12"
 
@@ -872,10 +871,6 @@ function CMegaDotaGameMode:OnGameRulesStateChange(keys)
 	if newState == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
 		if game_start then
 			game_start = false
-			Timers:CreateTimer(0.1, function()
-				GPM_Init()
-				return nil
-			end)
 		end
 	end
 end
