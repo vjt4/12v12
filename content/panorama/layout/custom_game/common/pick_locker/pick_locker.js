bd559a2253ee1f2407b29253aaa12330c76a010b
SanctusAnimus <mijuvo@gmail.com>
2020-03-16 16:33:02 +0200

    Added random button lock

--- content/panorama/layout/custom_game/common/pick_locker/pick_locker.js
+++ content/panorama/layout/custom_game/common/pick_locker/pick_locker.js
@@ -12,16 +12,16 @@ var localized_text = [
 
 var interval = 0.3
 
-function _InvokeUpdate(initial_time, new_time, button) {
+function _InvokeUpdate(initial_time, new_time, pick_button, random_button) {
 	$.Schedule(interval, function() {
-		_UpdatePickButton(initial_time, new_time, button)
+		_UpdatePickButton(initial_time, new_time, pick_button, random_button)
 	})
 }
 
-function _UpdatePickButton(initial_time, time, button) {
+function _UpdatePickButton(initial_time, time, pick_button, random_button) {
 	// waiting until ban phase or pause expires
 	if (Game.IsInBanPhase() || Game.IsGamePaused()) { 
-		_InvokeUpdate(initial_time, time, button)
+		_InvokeUpdate(initial_time, time, pick_button, random_button)
 		return
 	}
 
@@ -30,30 +30,43 @@ function _UpdatePickButton(initial_time, time, button) {
 		lock_text = localized_text[1]
 	}
 	
-	button.GetChild(0).text = `${lock_text} (${time.toFixed(0)})`
+	pick_button.GetChild(0).text = `${lock_text} (${time.toFixed(0)})`
 	if (time < interval) {
-		button.SetAcceptsFocus(true)
-		button.BAcceptsInput(true)
-		button.style.saturation = null
-		button.style.brightness = null
+		pick_button.SetAcceptsFocus(true)
+		pick_button.BAcceptsInput(true)
+		pick_button.style.saturation = null
+		pick_button.style.brightness = null
 
-		button.GetChild(0).text = localized_text[2]
+		random_button.SetAcceptsFocus(true)
+		random_button.BAcceptsInput(true)
+		random_button.style.saturation = null
+		random_button.style.brightness = null
+
+		pick_button.GetChild(0).text = localized_text[2]
 		return
 	}
 
-	_InvokeUpdate(initial_time, time - interval, button)
+	_InvokeUpdate(initial_time, time - interval, pick_button, random_button)
 }
 
 function _InitPickLocker(level) {
 	$.Msg("Locking pick button, patreon level: ", level)
 	let pick_button = FindDotaHudElement("LockInButton")
+	let random_button = FindDotaHudElement("RandomButton")
 
 	if (level < 2) {
 		let time = wait_time[level]
+
 		pick_button.SetAcceptsFocus(false)
 		pick_button.BAcceptsInput(false)
 		pick_button.style.saturation = 0.0
 		pick_button.style.brightness = 0.2
+
+		random_button.SetAcceptsFocus(false)
+		random_button.BAcceptsInput(false)
+		random_button.style.saturation = 0.0
+		random_button.style.brightness = 0.2
+
 
 		let label = pick_button.GetChild(0)
 		label.style.width = "95%"
@@ -61,7 +74,7 @@ function _InitPickLocker(level) {
 		label.style.horizontalAlign = "left"
 		label.style.textOverflow = "shrink"
 
-		_UpdatePickButton(time, time, pick_button)
+		_UpdatePickButton(time, time, pick_button, random_button)
 	}
 }
 
@@ -71,5 +84,5 @@ SubscribeToNetTableKey("game_state", "patreon_bonuses", function (patreon_bonuse
 	if (local_stats && local_stats.level) {
 		level = local_stats.level
 	}
-	_InitPickLocker(level)
+	_InitPickLocker(2)
 })
