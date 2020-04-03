020db1d12e2525f12ead128ee6301aeb941372c9
darklordabc <curtisjdubois@hotmail.com>
2020-04-03 19:27:21 +1100

    Added more tips and fixed some chinese words

--- content/panorama/scripts/custom_game/loading_screen.js
+++ content/panorama/scripts/custom_game/loading_screen.js
@@ -1,9 +1,45 @@
 // The tips we can show
 var tipsGeneral = [
-	{
-		img: 'file://{resources}/images/custom_game/tips_icon/tip_chat_wheel.png',
-		txt: '#LoadingTip_1'
-	},
+    {
+        img: 'file://{resources}/images/custom_game/tips_icon/tip_chat_wheel.png',
+        txt: '#LoadingTip_1'
+    },{
+        img: 'file://{resources}/images/custom_game/tips_icon/towers.png',
+        txt: '#LoadingTip_2'
+    },{
+        img: 'file://{resources}/images/custom_game/tips_icon/kicktroll.png',
+        txt: '#LoadingTip_3'
+    },{
+        img: 'file://{resources}/images/custom_game/tips_icon/respawn.png',
+        txt: '#LoadingTip_4'
+    },{
+        img: 'file://{resources}/images/custom_game/tips_icon/disablehelp.png',
+        txt: '#LoadingTip_5'
+    },{
+        img: 'file://{resources}/images/custom_game/tips_icon/mute.png',
+        txt: '#LoadingTip_6'
+    },{
+        img: 'file://{resources}/images/custom_game/tips_icon/raxes.png',
+        txt: '#LoadingTip_7'
+    },{
+        img: 'file://{resources}/images/custom_game/tips_icon/fountain.png',
+        txt: '#LoadingTip_8'
+    },{
+        img: 'file://{resources}/images/custom_game/tips_icon/itemtransfer.png',
+        txt: '#LoadingTip_9'
+    },{
+        img: 'file://{resources}/images/custom_game/tips_icon/perks.png',
+        txt: '#LoadingTip_10'
+    },{
+        img: 'file://{resources}/images/custom_game/tips_icon/nerfed.png',
+        txt: '#LoadingTip_11'
+    },{
+        img: 'file://{resources}/images/custom_game/tips_icon/helpus.png',
+        txt: '#LoadingTip_12'
+    },{
+        img: 'file://{resources}/images/custom_game/tips_icon/savefavs.png',
+        txt: '#LoadingTip_13'
+    },
 ];
 function print(q){
 	$.Msg(q)
