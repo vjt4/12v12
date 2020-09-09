daa0c6facb25d87cd3cd08764ffea2c717904f5b
Sheodar <kkabachok@gmail.com>
2020-09-09 03:54:11 +0300

    Added button "Hide/Show" for troll voting

--- content/panorama/layout/custom_game/common/voting_to_kick/voting_to_kick.js
+++ content/panorama/layout/custom_game/common/voting_to_kick/voting_to_kick.js
@@ -1,99 +1,135 @@
-var bVotingIsNow = false
+let bVotingIsNow = false;
 
-function print(data){
-	$.Msg(data)
-}
-function VotingToKickShowVoting(data){
-	print("SHOW VOTING")
-	var timerVoting = $.CreatePanel( "Panel", $( "#VotingToKickVoting" ), "VotingToKickCountdown" )
-	timerVoting.AddClass("Active")
-	var votingPanel = $("#VotingToKickVoting");
-	votingPanel.AddClass("Slide")
-	var targetPlayer = Game.GetPlayerInfo( data.playerId );
+function VotingToKickShowVoting(data) {
+	$.Msg("SHOW VOTING");
+	let timerVoting = $.CreatePanel("Panel", $("#VotingToKickVoting"), "VotingToKickCountdown");
+	timerVoting.AddClass("Active");
+	let votingPanel = $("#VotingToKickVoting");
+	votingPanel.AddClass("Slide");
+	let targetPlayer = Game.GetPlayerInfo(data.playerId);
 
-	$("#VotingToKickVotingReasonText").text = $.Localize("#voting_to_kick_reason_tooltip")+": "+$.Localize("#voting_to_kick_reason_"+data.reason)
+	votingPanel.SetHasClass("Hide", false);
+	$("#HideVotingWrap").SetHasClass("Reverse", false);
 
-	var votingPanelHideText = $("#VotingToKickVotingHeadText");
-	votingPanelHideText.html=true
-	votingPanelHideText.text =  $.Localize("#voting_to_kick_tooltip")+" "+"<font color='#ed1313'>" + targetPlayer.player_name + "</font>"+" ?";
-	var votingPanelModelHero = $("#VotingToKickVotingHeroModel");
-	$("#VotingToKickModelPanel").BCreateChildren("<DOTAScenePanel hittest=\"false\" id=\"VotingToKickVotingHeroModel\" style=\"width:210px;height:210px;\" unit=\""+targetPlayer.player_selected_hero+"\" particleonly=\"false\"/>")
+	$("#VotingToKickVotingReasonText").text =
+		$.Localize("#voting_to_kick_reason_tooltip") + ": " + $.Localize("#voting_to_kick_reason_" + data.reason);
 
+	let votingPanelHideText = $("#VotingToKickVotingHeadText");
+	votingPanelHideText.html = true;
+	votingPanelHideText.text =
+		$.Localize("#voting_to_kick_tooltip") +
+		" " +
+		"<font color='#ed1313'>" +
+		targetPlayer.player_name +
+		"</font>" +
+		" ?";
+	$("#VotingToKickModelPanel").BCreateChildren(
+		'<DOTAScenePanel hittest="false" id="VotingToKickVotingHeroModel" style="width:210px;height:210px;" unit="' +
+			targetPlayer.player_selected_hero +
+			'" particleonly="false"/>',
+	);
 
-	$("#VotingToKickKDA").html=true
-	$("#VotingToKickKDA").text = $.Localize("#voting_to_kick_kda_tooltip")+"\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0"+Players.GetKills(data.playerId)+" / "+Players.GetDeaths(data.playerId)+" / "+Players.GetAssists(data.playerId)
+	$("#VotingToKickKDA").html = true;
+	$("#VotingToKickKDA").text =
+		$.Localize("#voting_to_kick_kda_tooltip") +
+		"\u00A0".repeat(8) +
+		Players.GetKills(data.playerId) +
+		" / " +
+		Players.GetDeaths(data.playerId) +
+		" / " +
+		Players.GetAssists(data.playerId);
 
-	if(Game.GetLocalPlayerID() != data.playerIdInit){
-		$("#VotingToKickVotingYes").visible = true
-    	$("#VotingToKickVotingNo").visible = true
-	}else{
-		$("#VotingToKickVotingYes").visible = false
-		$("#VotingToKickVotingNo").visible = false
+	if (Game.GetLocalPlayerID() != data.playerIdInit) {
+		$("#VotingToKickVotingYes").visible = true;
+		$("#VotingToKickVotingNo").visible = true;
+	} else {
+		$("#VotingToKickVotingYes").visible = false;
+		$("#VotingToKickVotingNo").visible = false;
 	}
 
-	if(data.playerVoted != null){
-		$("#VotingToKickVotingYes").visible = false
-		$("#VotingToKickVotingNo").visible = false
+	if (data.playerVoted != null) {
+		$("#VotingToKickVotingYes").visible = false;
+		$("#VotingToKickVotingNo").visible = false;
 	}
 }
+function ToggleVotingPanel() {
+	$("#VotingToKickVoting").ToggleClass("Hide");
+	const hideButton = $("#HideVotingWrap");
+	hideButton.ToggleClass("Reverse");
 
+	hideButton.SetPanelEvent("onmouseover", function () {
+		$.DispatchEvent(
+			"DOTAShowTextTooltip",
+			hideButton,
+			$.Localize(hideButton.BHasClass("Reverse") ? "show_voting" : "hide_voting"),
+		);
+	});
+	hideButton.SetPanelEvent("onmouseout", function () {
+		$.DispatchEvent("DOTAHideTextTooltip");
+	});
+}
+function VotingToKickHideVoting(data) {
+	$.Msg("HIDE VOTING");
+	$("#VotingToKickVotingYes").visible = false;
+	$("#VotingToKickVotingNo").visible = false;
+	$("#VotingToKickCountdown").DeleteAsync(0);
 
-function VotingToKickHideVoting(data){
-	print("HIDE VOTING")
-	$("#VotingToKickVotingYes").visible = false
-	$("#VotingToKickVotingNo").visible = false
-	$("#VotingToKickCountdown").DeleteAsync(0)
-
-	var votingPanel = $("#VotingToKickVoting");
-	votingPanel.RemoveClass("Slide")
+	let votingPanel = $("#VotingToKickVoting");
+	votingPanel.RemoveClass("Slide");
 }
 
-function VotingToKickVoteYes(data){
-	print("YES VOTING")
+function VotingToKickVoteYes(data) {
+	$.Msg("YES VOTING");
 	GameEvents.SendCustomGameEventToServer("voting_to_kick_vote_yes", {});
-	$("#VotingToKickVotingYes").visible = false
-	$("#VotingToKickVotingNo").visible = false
+	$("#VotingToKickVotingYes").visible = false;
+	$("#VotingToKickVotingNo").visible = false;
 }
 
-function VotingToKickVoteNo(data){
-	print("NO VOTING")
+function VotingToKickVoteNo(data) {
+	$.Msg("NO VOTING");
 	GameEvents.SendCustomGameEventToServer("voting_to_kick_vote_no", {});
-	$("#VotingToKickVotingYes").visible = false
-	$("#VotingToKickVotingNo").visible = false
+	$("#VotingToKickVotingYes").visible = false;
+	$("#VotingToKickVotingNo").visible = false;
 }
 
-function VotingToKickHideReason(){
-	print("HIDE REASON")
-	var reasonPanel = $("#VotingToKickReasonPanel");
-	reasonPanel.RemoveClass("Slide")
+function VotingToKickHideReason() {
+	$.Msg("HIDE REASON");
+	let reasonPanel = $("#VotingToKickReasonPanel");
+	reasonPanel.RemoveClass("Slide");
 }
-function VotingToKickShowReason(data){
-	print("SHOW REASON")
-	var reasonPanel = $("#VotingToKickReasonPanel");
-	reasonPanel.AddClass("Slide")
+function VotingToKickShowReason(data) {
+	$.Msg("SHOW REASON");
+	let reasonPanel = $("#VotingToKickReasonPanel");
+	reasonPanel.AddClass("Slide");
 
-	var reasonPanelHideText = $("#VotingToKickReasonHeadPanelText");
-	reasonPanelHideText.html=true
-	var targetPlayer = Game.GetPlayerInfo( data.playerId );
-	reasonPanelHideText.text = $.Localize("#voting_to_kick_choose_reason_tooltip")+" "+"<font color='#23d923'>" + targetPlayer.player_name + "</font>";
-
+	let reasonPanelHideText = $("#VotingToKickReasonHeadPanelText");
+	reasonPanelHideText.html = true;
+	let targetPlayer = Game.GetPlayerInfo(data.playerId);
+	reasonPanelHideText.text =
+		$.Localize("#voting_to_kick_choose_reason_tooltip") +
+		" " +
+		"<font color='#23d923'>" +
+		targetPlayer.player_name +
+		"</font>";
 }
-function VotingToKickInitVoting(reason){
-	print("PICK REASON")
- 	GameEvents.SendCustomGameEventToServer("voting_to_kick_reason_is_picked", {reason:reason});
+function VotingToKickInitVoting(reason) {
+	$.Msg("PICK REASON");
+	GameEvents.SendCustomGameEventToServer("voting_to_kick_reason_is_picked", { reason: reason });
 }
-function VotingToKickDebugPring(data){
-	var playerVoted = Game.GetPlayerInfo( data.playerVotedId );
-	print(playerVoted.player_name + " VOTE: " + data.vote + "NEED TOTAL: " + data.total)
+function VotingToKickDebugPring(data) {
+	let playerVoted = Game.GetPlayerInfo(data.playerVotedId);
+	$.Msg(playerVoted.player_name + " VOTE: " + data.vote + "NEED TOTAL: " + data.total);
 }
-function VotingToKickInit(){
+function VotingToKickInit() {
 	GameEvents.SendCustomGameEventToServer("voting_to_kick_check_voting_state", {});
-	GameEvents.Subscribe('voting_to_kick_show_reason', VotingToKickShowReason);
-	GameEvents.Subscribe('voting_to_kick_hide_reason', VotingToKickHideReason);
+	GameEvents.Subscribe("voting_to_kick_show_reason", VotingToKickShowReason);
+	GameEvents.Subscribe("voting_to_kick_hide_reason", VotingToKickHideReason);
 
-	GameEvents.Subscribe('voting_to_kick_show_voting', VotingToKickShowVoting);
-	GameEvents.Subscribe('voting_to_kick_hide_voting', VotingToKickHideVoting);
+	GameEvents.Subscribe("voting_to_kick_show_voting", VotingToKickShowVoting);
+	GameEvents.Subscribe("voting_to_kick_hide_voting", VotingToKickHideVoting);
 
-	GameEvents.Subscribe('voting_to_kick_debug_print', VotingToKickDebugPring);
+	GameEvents.Subscribe("voting_to_kick_debug_$.Msg", VotingToKickDebugPring);
+
+	VotingToKickShowVoting({ reason: "feeding", playerId: 1, playerIdInit: 0 });
 }
 VotingToKickInit();
