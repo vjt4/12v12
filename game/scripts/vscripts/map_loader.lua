05171561bfa754bd73b0aef839445029b72de26a
SanctusAnimus <mijuvo@gmail.com>
2021-02-05 13:49:10 +0200

    force refresh for hero positions on map load

--- game/scripts/vscripts/map_loader.lua
+++ game/scripts/vscripts/map_loader.lua
@@ -20,4 +20,11 @@ end
 function MapLoader:OnMapLoadingFinished(spawngroup_handle)
 	print("[Map Loader] finished loading map with spawngroup", spawngroup_handle)
 	-- if anything needs re-activating after map was loaded, it should go here
+	Timers:CreateTimer(0.1, function()
+		for i, hero in pairs(HeroList:GetAllHeroes()) do
+			if IsValidEntity(hero) then
+				FindClearSpaceForUnit(hero, hero:GetAbsOrigin(), false)
+			end
+		end
+	end)
 end
